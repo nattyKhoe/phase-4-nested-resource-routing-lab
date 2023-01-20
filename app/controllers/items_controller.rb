e4b98bb54ed4b7c460b4db97ba9650220c30fea0
Natasha <natasha.khoe@gmail.com>
2023-01-20 18:49:24 +0800

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
+
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = find_user.items.create(item_params)
+    render json: item, include: :user, status: :created
+  end
+
+  private
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def find_user
+    User.find(params[:user_id])
   end
 
 end
